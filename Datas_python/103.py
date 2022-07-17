c4fc3bf9543ebb4ed18a72c990921c27ab4973d4
Gilberto Tezuka <90873996+gibatzk@users.noreply.github.com>
2022-07-17 18:04:31 -0300

    Add files via upload

---
+++ Datas_python/103.py
@@ -0,0 +1,8 @@
+import datetime
+
+x = datetime.datetime(2020, 5, 17, 23, 12, 30)
+y = datetime.datetime(2021, 7, 24, 20, 30, 50)
+z = y - x
+print(x)
+print(y)
+print(z)
---
+++ Datas_python/104.py
@@ -0,0 +1,8 @@
+import datetime
+
+x = datetime.datetime(2018, 6, 1)
+
+print(x)
+
+y = x.strftime('%A, %d %B %Y')
+print(y)
