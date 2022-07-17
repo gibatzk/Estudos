fed45b399b516c4dcc4e7c084e292dbf5f6aec4c
gibatzk <gibatzk@hotmail.com>
2022-07-17 18:22:45 -0300

    Create 110.py

---
+++ Json/110.py
@@ -0,0 +1,22 @@
+import json
+
+x = {
+    "nome":"Gilberto",
+    "idade":36,
+    "casado":True,
+    "divorciado":False,
+    "filho":None,
+    "pet":"Robin",
+    "carro": [
+        {"modelo":"fiat Fiorino", "cor":"branca"},
+        {"modelo":"Peuget 504", "cor":"prata"}
+    ]
+}
+
+#print(json.dumps(x, indent=4))
+#print(json.dumps(x, indent=4, separators=(". ", " = ")))
+#y = json.dumps(x, indent=4, separators=(". ", " = "), sort_keys=True)
+y = json.dumps(x, separators=(". ", " = "), sort_keys=True)
+
+
+print(y)
