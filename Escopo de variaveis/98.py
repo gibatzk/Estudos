4ba5082a0768ba4bcee4aac3c0dcb8f1cec11e86
gibatzk <gibatzk@hotmail.com>
2022-07-24 17:59:31 -0300

    adicionando arquivo

---
+++ Escopo de variaveis/98.py
@@ -0,0 +1,25 @@
+x = 300
+
+#def myFunc():
+ #    print(x)
+
+#def myFunc():
+#   x = 200
+#   print(x)
+
+#def myFunc():
+#    global x
+#    x = 300
+
+def myFunc():
+   global x
+   x = 200
+   print(x)
+
+
+myFunc()
+print(x)
+
+
+
+
