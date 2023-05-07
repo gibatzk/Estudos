1b31be53595411006c08d6e0d29db10bc91f1550
Gilberto Tezuka <90873996+gibatzk@users.noreply.github.com>
2023-05-06 21:15:53 -0300

    Add files via upload

---
+++ estudos_py/O.O.py
@@ -0,0 +1,11 @@
+numero = 123
+titular = 'Gilberto'
+saldo = 55.0
+limite = 1000.0
+
+conta = {"numero": 123, "titular": 'Gilberto', 'saldo': 55.0, 'limite': 1000.9}
+conta['numero']
+print(numero)
+conta2 = {"numero": 321, "titular": 'Robin', 'saldo': 100.0, 'limite': 1000.9}
+conta2['numero']
+print(numero)
---
+++ estudos_py/classe_retangulo.py
@@ -0,0 +1,9 @@
+class Retangulo:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.area = x * y
+
+    def obter_area(self):
+        return self.area
+        
